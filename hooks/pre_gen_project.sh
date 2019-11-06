9b3f514df457a274f8854c679cb2f01302777af8
Matthieu Adjogah <matthieu.adjogah@travelex.com>
2019-11-06 08:29:54 +0000

    adding cookiecutter variables output fix

--- hooks/pre_gen_project.sh
+++ hooks/pre_gen_project.sh
@@ -2,7 +2,6 @@
 
 cat > /var/tmp/set-vars.sh <<EOF1
 #!/usr/bin/env bash
-echo export PROJECT_NAME={{ cookiecutter.project_name }}
-echo export FUNCTION_FOLDER={{ cookiecutter.function_name|lower }}
+export PROJECT_NAME="{{ cookiecutter.project_name }}"
+export FUNCTION_FOLDER="{{ cookiecutter.function_name|lower }}"
 EOF1
-
